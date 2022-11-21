ea3a560ea302e2c1b08afd2d2448faeb411ea866
Miroslav Martynov <marty.mircha@gmail.com>
2022-11-21 14:21:55 +0100

    update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -40,6 +40,75 @@ async function seed() {
     },
   });
 
+  await prisma.category.create({
+    data: {
+      name: "Personal",
+      color: "#FF0000",
+      userId: user.id,
+    },
+  });
+
+  const categoryGroceries = await prisma.category.create({
+    data: {
+      name: "Groceries",
+      color: "#00FF00",
+      userId: user.id,
+    },
+  });
+
+  await prisma.category.create({
+    data: {
+      name: "Gifts",
+      color: "#0000FF",
+      userId: user.id,
+    },
+  });
+
+  await prisma.category.create({
+    data: {
+      name: "Health",
+      color: "#FFFF00",
+      userId: user.id,
+    },
+  });
+
+  await prisma.category.create({
+    data: {
+      name: "Work",
+      color: "#FF00FF",
+      userId: user.id,
+    },
+  });
+
+  await prisma.record.create({
+    data: {
+      info: "Lidl",
+      date: new Date("2021-01-01"),
+      value: -100.23,
+      userId: user.id,
+      categoryId: categoryGroceries.id,
+    },
+  });
+
+  await prisma.record.create({
+    data: {
+      info: "Aldi",
+      date: new Date("2021-01-02"),
+      value: -200.23,
+      userId: user.id,
+      categoryId: categoryGroceries.id,
+    },
+  });
+
+  await prisma.record.create({
+    data: {
+      info: "Salary",
+      date: new Date("2021-01-03"),
+      value: 45000,
+      userId: user.id,
+    },
+  });
+
   console.log(`Database has been seeded. 🌱`);
 }
 
