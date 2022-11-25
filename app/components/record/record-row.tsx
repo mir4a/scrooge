6481c1007d804526c2ab027a770d5fb2814d95fa
Miroslav Martynov <marty.mircha@gmail.com>
2022-11-25 14:46:37 +0100

    clickable row for records

--- app/components/record/record-row.tsx
+++ app/components/record/record-row.tsx
@@ -2,14 +2,24 @@ import type { Record } from "./record-table";
 import { toLocaleDate } from "~/utils/date-time-formatter";
 import Currency from "../currency";
 import ColourIndicator from "../category/colour-indicator";
+import { useNavigate } from "@remix-run/react";
 
 export interface RecordRowProps {
   record: Record;
 }
 
 export default function RecordRow({ record }: RecordRowProps) {
+  const navigate = useNavigate();
+
+  const handleRowClick = () => {
+    navigate(`/records/${record.id}`);
+  };
+
   return (
-    <div className="table-row">
+    <div
+      className="group table-row cursor-pointer hover:bg-slate-50"
+      onClick={handleRowClick}
+    >
       <div className="table-cell border-b border-slate-100 p-4 pl-8 text-slate-500 dark:border-slate-700 dark:text-slate-400">
         {toLocaleDate(record.date)}
       </div>
