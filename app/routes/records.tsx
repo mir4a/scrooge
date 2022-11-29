85993a9ce3fc76eb97d31943a1e75e79f5d93418
Miroslav Martynov <marty.mircha@gmail.com>
2022-11-29 22:33:53 +0100

    change layout for records

--- app/routes/records.tsx
+++ app/routes/records.tsx
@@ -7,6 +7,7 @@ import { useUser } from "~/utils";
 import { getRecords } from "~/models/record.server";
 import Header from "~/components/layout/header";
 import RecordTable from "~/components/record/record-table";
+import MainLayout from "~/components/layout/main";
 
 export async function loader({ request }: LoaderArgs) {
   const userId = await requireUserId(request);
@@ -22,25 +23,26 @@ export default function RecordsPage() {
     <>
       <Header username={user.email} />
 
-      <div className="grid grid-cols-12 gap-4 py-8">
-        <div className="col-span-5 col-start-2">
-          <Link
-            to="new"
-            className="Button Button--primary mb-10 block w-fit justify-self-end py-3 px-8 text-center text-xl font-bold"
-          >
-            + New Record
-          </Link>
-
-          {data.records.length === 0 ? (
-            <p className="p-4">No records yet</p>
-          ) : (
-            <RecordTable records={data.records} />
-          )}
+      <MainLayout>
+        <div className="grid grid-cols-4 gap-4">
+          <div className="">
+            <Link
+              to="new"
+              className="Button Button--primary mb-10 block w-fit justify-self-end py-3 px-8 text-center text-xl font-bold"
+            >
+              + New Record
+            </Link>
+          </div>
+          <div className="col-span-3 mb-8">
+            <Outlet />
+          </div>
         </div>
-        <div className="col-span-4 col-start-8">
-          <Outlet />
-        </div>
-      </div>
+        {data.records.length === 0 ? (
+          <p className="p-4">No records yet</p>
+        ) : (
+          <RecordTable records={data.records} />
+        )}
+      </MainLayout>
     </>
   );
 }
